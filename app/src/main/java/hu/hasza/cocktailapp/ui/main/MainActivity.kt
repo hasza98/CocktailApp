f491d772d8907741fcdd4f5c23f7d672bcd9c03f
Hadarics Szabolcs <hasza98@gmail.com>
2022-04-10 21:51:31 +0200

    Added Hilt

--- app/src/main/java/hu/hasza/cocktailapp/ui/main/MainActivity.kt
+++ app/src/main/java/hu/hasza/cocktailapp/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package hu.hasza.cocktailapp
+package hu.hasza.cocktailapp.ui.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import hu.hasza.cocktailapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
